6f6b401ab799a28280f46cacc63083b227af2696
isaiascuvula <bersyteinfo@gmail.com>
2024-11-21 14:10:30 +0200

    Added AppDbContext

---
+++ ShopApi/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ShopApi.Models;
+
+namespace ShopApi.Data;
+
+public class AppDbContext: DbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+    
+    public DbSet<Product> Products { get; set; }
+    public DbSet<CustomerProduct> CustomerProducts { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Sales> Sales { get; set; }
+    
+}
