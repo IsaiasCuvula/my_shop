0739bd0601843f13f18e35b260b63972314f2064
isaiascuvula <bersyteinfo@gmail.com>
2024-11-22 01:44:02 +0200

    Updated orders table

---
+++ ShopApi/Migrations/20241121234313_FixOrdersTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixOrdersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupOrderId",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupOrderId",
+                table: "Orders");
+        }
+    }
+}
