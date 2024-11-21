bf3e61ac1de9ef199be40cc10da8a9ff867edef4
isaiascuvula <bersyteinfo@gmail.com>
2024-11-21 23:31:55 +0200

    quantity available in stock

--- ShopApi/Models/Product.cs
+++ ShopApi/Models/Product.cs
@@ -9,6 +9,7 @@ public class Product
     public required string Description { get; set; }
     public decimal Price { get; set; }
     public DateTime ExpirationDate { get; set; }
+    //This is the quantity available in stock
     public int Quantity { get; set; }
     public long ProductNumber { get; set; }
     public required string Image { get; set; }
