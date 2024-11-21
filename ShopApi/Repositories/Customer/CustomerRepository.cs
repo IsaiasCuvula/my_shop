310e7577ca44b1cb9673474930a72071254f9e6b
isaiascuvula <bersyteinfo@gmail.com>
2024-11-21 14:39:23 +0200

    Added ICustomerRepository and CustomerRepository Implementation

---
+++ ShopApi/Repositories/Customer/CustomerRepository.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using ShopApi.Data;
+using ShopApi.Models;
+
+namespace ShopApi.Repositories;
+
+public class CustomerRepository: ICustomerRepository
+{
+    private readonly AppDbContext _context;
+
+    public CustomerRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Customer?> GetByIdAsync(long id)
+    {
+     return await _context.Customers.FirstOrDefaultAsync(c=> c.Id==id);
+    }
+
+    public  async Task<IEnumerable<Customer>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync();
+    }
+
+    public  async Task AddAsync(Customer customer)
+    {
+       await _context.Customers.AddAsync(customer);
+       await _context.SaveChangesAsync();
+    }
+
+    public async  Task UpdateAsync(Customer customer)
+    {
+         _context.Customers.Update(customer);
+        await _context.SaveChangesAsync();
+    }
+
+    public async  Task DeleteAsync(Customer customer)
+    {
+         _context.Customers.Remove(customer);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ ShopApi/Repositories/Customer/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+using ShopApi.Models;
+
+namespace ShopApi.Repositories;
+
+public interface ICustomerRepository
+{
+    Task<Customer?> GetByIdAsync(long id);
+    Task<IEnumerable<Customer>> GetAllAsync();
+    Task AddAsync(Customer customer);
+    Task UpdateAsync(Customer customer);
+    Task DeleteAsync(Customer customer);
+}
