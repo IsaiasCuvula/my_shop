2a8f06a0c7f26c9b9c5c6bbe5d2f51ab21ccd453
isaiascuvula <bersyteinfo@gmail.com>
2024-11-22 01:09:05 +0200

    Code organization

--- ShopApi/Repositories/Product/IProductRepository.cs
+++ ShopApi/Repositories/Product/IProductRepository.cs
@@ -6,8 +6,8 @@ public interface IProductRepository
 {
     Task<Product?> GetByIdAsync(long id);
     Task<Product?> GetByNumberAsync(long productNumber);
-    Task<List<Product>> GetAllAsync();
     Task<Product> AddAsync(Product customer);
     Task<Product> UpdateAsync(Product customer);
     Task DeleteAsync(Product customer);
+    Task<List<Product>> GetAllAsync();
 }
